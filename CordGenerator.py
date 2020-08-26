249c573aed2ccff669ea8dc6b7e15c7fa00a76bd
Safewaan <Safewaan@users.noreply.github.com>
2020-08-25 20:12:02 -0400

    Update CordGenerator.py
    - Fixed certain cords not generating.

--- CordGenerator.py
+++ CordGenerator.py
@@ -23,13 +23,15 @@ def CordGen(gD, width, height):
     cords = []
 
     #Main loop
-    for y in range(gD - 1):
-        incrementYTotal += incrementY
-        for x in range(gD - 1):
-            incrementXTotal += incrementX
+    for y in range(gD):
+        for x in range(gD):
             cords.append([incrementXTotal, incrementYTotal])
-
+            incrementXTotal += incrementX
+            
+        incrementYTotal += incrementY
         incrementXTotal = 0
 
     #Return
     return (cords)
+
+print (CordGen(5, 800, 800))
